e9c67e1a675d2038df2cafe9cf95ccc76850b4b2
xiaoyi0927 <51571108+xiaoyi0927@users.noreply.github.com>
2021-06-15 15:58:05 +0800

    Add files via upload

---
+++ 0513/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+class App extends React.Component {
+  constructor(props) {
+     super(props);
+     this.state = {data: '預設資料...'};
+  };
+  updateState = (e) => {
+     this.setState({data: e.target.value});
+  }
+  render() {
+     return (
+        <div>
+           <Content myDataProp = {this.state.data} updateStateProp = {this.updateState} />
+        </div>
+     );
+  }
+}
+class Content extends React.Component {
+  render() {
+     return (
+        <div>
+           <input type = "text" value = {this.props.myDataProp} onChange = {this.props.updateStateProp} />
+           <h3>{this.props.myDataProp}</h3>
+        </div>
+     );
+  }
+}
+export default App;
