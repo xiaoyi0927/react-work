92b1503a93a0963354c286254e27a44f1ca6759b
xiaoyi0927 <51571108+xiaoyi0927@users.noreply.github.com>
2021-06-15 19:23:55 +0800

    Add files via upload

---
+++ 0527/App.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import DeleteIcon from '@material-ui/icons/Delete';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import TextField from '@material-ui/core/TextField';
+import {makeStyles,ThemeProvider,createMuiTheme} from '@material-ui/core/Styles'
+import {orange,green} from '@material-ui/core/colors'
+import 'fontsource-roboto';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import AppBar from '@material-ui/core/AppBar';
+import ToolBar from '@material-ui/core/ToolBar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/core/Menu';
+const useStyles=makeStyles({
+  root:{
+    background:'linear-gradient(45deg,#FE6B8B,#FF8E53)',
+    border:0,
+    marginBottom:15,
+    borderRadius:15,
+    color: 'white',
+    padding:'5px 30px'
+  }
+})
+
+const theme=createMuiTheme({
+  typography:{
+    h2:{
+      fontSize:36,
+    }
+  },
+  palette:{
+    primary:{
+      main:green[400]
+    },
+    secondary:{
+      main:orange[400]
+    }
+  }
+})
+
+function ButtonStyle(){
+  const classes=useStyles();
+  return <Button className={classes.root}>Test Style Button</Button>
+}
+
+
+function CheckboxExample () {
+  const [checked,setChecked] = React.useState(true)
+  return (
+    <FormControlLabel
+      control={<Checkbox
+      checked={checked}
+      icon={<DeleteIcon />}
+      checkedIcon={<SaveIcon />}
+      onChange={(e)=>setChecked(e.target.checked)}
+      inputProps={{
+        'aria-label': 'secondary checkbox'
+      }}
+    />}
+      label="Testing Checkbox"
+  />
+  )
+}
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Container maxWidth="xs">
+    <div className="App">
+      <header className="App-header">
+        <AppBar color="secondary">
+          <ToolBar>
+            <IconButton>
+              <MenuIcon/>
+            </IconButton>
+            <Typography variant="h6">
+              MUI Themeing
+            </Typography>
+            <Button>
+              Login
+            </Button>
+          </ToolBar>
+        </AppBar>
+      <Typography variant="h2" componet="div">
+        Welcome to MUI
+      </Typography>
+      <Typography variant="subtitle">
+        learn how to use MUI
+      </Typography>
+      <ButtonStyle/>
+      <Grid container spacing={2} justify="center">
+        <Grid item xs={3} sm={6} >
+        <Paper style={{height:75,width:'100%',}}></Paper>
+        </Grid>
+        <Grid item xs={3} sm={6}>
+        <Paper style={{height:75,width:'100%',}}></Paper>
+        </Grid>
+        <Grid item xs={3} sm={6}>
+        <Paper style={{height:75,width:'100%',}}></Paper>
+        </Grid>
+      </Grid>
+        <TextField
+         variant="filled"
+         color="secondary"
+         type="email"
+         label="The Time"
+         placeholder="test@test.com"
+         />
+        <CheckboxExample/>
+        <ButtonGroup variant="contained" color="primary">
+        <Button 
+        startIcon={<SaveIcon />}>
+        Save  
+        </Button>
+        <Button 
+        starIcon={<DeleteIcon />}>
+        Discard  
+        </Button>
+        </ButtonGroup>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+    </Container>
+    </ThemeProvider> 
+  );
+}
+
+export default App ;
