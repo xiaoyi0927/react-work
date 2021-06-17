9a3a2c723dde14e969de0820838fbcad5cba01fb
xiaoyi0927 <51571108+xiaoyi0927@users.noreply.github.com>
2021-06-17 10:26:29 +0800

    Add files via upload

---
+++ 0408/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Comment from './App';
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'https://placekitten.com/g/64/64',
+    },
+  };
+  
+ReactDOM.render(
+  <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />,
+  document.getElementById('root')
+);
